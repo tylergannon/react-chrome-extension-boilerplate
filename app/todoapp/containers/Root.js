743bde4b4b86ae4143ee35862fd6d2d0c6a7d681
Tyler Gannon <tgannon@gmail.com>
2016-01-09 18:51:23 +0800

    Successfully load router and redux router.

--- app/todoapp/containers/Root.js
+++ app/todoapp/containers/Root.js
@@ -2,6 +2,24 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import TodoApp from './TodoApp';
 
+import { createStore, combineReducers } from 'redux'
+import { Router, Route } from 'react-router'
+import { createHistory } from 'history'
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router'
+
+// import reducers from '<project-path>/reducers'
+
+import todos from '../reducers/todos';
+
+const reducer = combineReducers(Object.assign({}, todos, {
+  routing: routeReducer
+}))
+const store = createStore(reducer)
+const history = createHistory()
+
+syncReduxAndRouter(history, store)
+
+
 class AppContainer extends Component {
   render() {
     return (
